eaa437ae08d73e9414f77b62d5f8838e5ffdb9a1
chohangul <72744225+chohangul@users.noreply.github.com>
2021-08-24 21:40:41 +0900

    const_and_let

--- practice/app.js
+++ practice/app.js
@@ -1,1 +1,15 @@
-alert("hi");
+// const a = 5;
+// const b = 2;
+// const myName = "hangeul"
+const a = 5;
+const b = 2;
+let myName = "hangeul"
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "JJogeul"
+
+console.log("hello " + myName)
