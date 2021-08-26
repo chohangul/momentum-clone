00b7ddc41eb8f3806035ee69028e3ef21d82eca2
chohangul <72744225+chohangul@users.noreply.github.com>
2021-08-26 22:09:59 +0900

    querySelector

--- practice/app.js
+++ practice/app.js
@@ -1,13 +1,3 @@
-// Conditionals
+const title = document.querySelector(".hello h1");
 
-const age = parseInt(prompt("How old are you?"));
-
-// console.log(isNaN(age));
-
-if (isNaN(age)) {
-  console.log("Please write a number");
-} else if (age < 18) {
-  console.log("you are too young");
-} else {
-  console.log("Thank you for writing your age");
-}
+console.log(title);
