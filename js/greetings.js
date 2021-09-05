96e9f40a46052b63741b9476904b90becc5f47b2
chohangul <72744225+chohangul@users.noreply.github.com>
2021-09-05 23:06:39 +0900

    update_weather_hidden

--- js/greetings.js
+++ js/greetings.js
@@ -3,6 +3,28 @@ const loginInput = document.querySelector("#login-form input");
 const loginSubmit = document.querySelector("#login-form input:last-child");
 const greeting = document.querySelector("#greeting");
 
+const API_KEY = "cea4f660385657856be00cbe5ed39e44";
+const weather = document.querySelector("#weather span:last-child");
+const city = document.querySelector("#weather span:first-child");
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerText = `You are in ${data.name} now.`;
+      weather.innerText = `And Today's weather is ${data.weather[0].main} (${data.main.temp}°C)`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
@@ -11,6 +33,8 @@ function onLoginSubmit(event) {
   loginForm.classList.add(HIDDEN_CLASSNAME);
   loginInput.classList.add(HIDDEN_CLASSNAME);
   loginSubmit.classList.add(HIDDEN_CLASSNAME);
+  weather.classList.remove(HIDDEN_CLASSNAME);
+  city.classList.remove(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
@@ -19,6 +43,8 @@ function onLoginSubmit(event) {
 function paintGreetings(username) {
   greeting.innerText = `Hello, ${username} !`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  weather.classList.remove(HIDDEN_CLASSNAME);
+  city.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
